35f94a438d57d7f21ae40dc344b955bceead4787
Ivy <ivy@IvydeMacBook-Pro.local>
2018-01-15 14:29:33 +0800

    添加element汇总

---
+++ test/element.js
@@ -0,0 +1,48 @@
+// 知识库
+const common = {
+    createUnitButton : '.ant-btn button___3jJ6D editable-add-btn ant-btn-primary',
+    edit : '.anticon  anticon-edit undefined',
+    delete : '.anticon  anticon-delete undefined',
+    deleteAndYes : '.ant-btn ant-btn-primary ant-btn-sm',
+    deleteAndNo : '.ant-btn ant-btn-sm',
+    completeButton : '.ant-btn ant-btn-primary',
+    cancleButton : 'ant-btn ant-btn-ghost',
+    
+}
+
+// 单位
+const unit = {
+    createUnitButton : '.ant-btn button___3jJ6D editable-add-btn ant-btn-primary',
+    edit : '.anticon  anticon-edit undefined',
+    delete : '.anticon  anticon-delete undefined',
+    deleteAndYes : '.ant-btn ant-btn-primary ant-btn-sm',
+    deleteAndNo : '.ant-btn ant-btn-sm',
+    name : '#name',
+    note : '#note',
+    completeButton : '.ant-btn ant-btn-primary',
+    cancleButton : 'ant-btn ant-btn-ghost',
+    search : '.ant-input',
+
+};
+//产能标准
+const productivityStandard = {
+    createProductivityStandardButton : '.ant-btn button___3jJ6D editable-add-btn ant-btn-primary',
+    edit : '.anticon  anticon-edit undefined',
+    delete : '.anticon  anticon-delete undefined',
+}
+
+const qcItem = {
+    createAttationButton : '.ant-btn button___3jJ6D editable-add-btn ant-btn-primary',
+    edit : '.anticon  anticon-edit undefined',
+    delete : '.anticon  anticon-delete undefined',
+    deleteAndYes : '.ant-btn ant-btn-primary ant-btn-sm',
+    deleteAndNo : '.ant-btn ant-btn-sm',
+    // 创建质检分类和创建关注点的输入框中名称的id
+    name : '#name',
+    completeButton : '.ant-btn ant-btn-primary',
+    cancleButton : 'ant-btn ant-btn-ghost',
+    attationInput : '.ant-input',
+    addAttation : 'anticon  anticon-plus-circle-o undefined',
+    deleteAttation : '.anticon  anticon-minus-circle-o delIcon___2mNrC'
+}
+
